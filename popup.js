3299ec32713d9f0615748219a294025850f41ae5
tahsin_dev <tahsinahmed52225@gmail.com>
2021-10-06 13:34:21 +0600

    first commit

---
+++ popup.js
@@ -0,0 +1,25 @@
+// Snag our button
+let btn = document.getElementById("changeFont")
+
+// Set our button's color to the color that we stored
+chrome.storage.sync.get("color", ({ color }) => {
+    btn.style.backgroundColor = color
+})
+
+// Run on click
+btn.addEventListener("click", async () => {
+    let value = document.getElementById("input_text").value;
+    alert(value);
+
+    // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }) // Find current tab
+
+    // chrome.scripting.executeScript({ // Run the following script on our tab
+    //     target: { tabId: tab.id },
+    //     function: () => {
+    //         let elems = document.querySelectorAll("*"); // Grab every element in the dom
+    //         for (var i = 0; i < elems.length; i++) {
+    //             elems[i].style.fontFamily = "Comic Sans MS";
+    //         }
+    //     }
+    // })
+})
